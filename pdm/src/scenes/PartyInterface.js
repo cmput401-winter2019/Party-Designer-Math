8e4b110216d1286cca7e1ea5b607c9486fef8d82
HailanXyouknow <hailan@ualberta.ca>
2019-02-22 16:12:53 -0700

    removed node folder

--- pdm/src/scenes/PartyInterface.js
+++ pdm/src/scenes/PartyInterface.js
@@ -16,11 +16,11 @@ export class PartyInterface extends Phaser.Scene {
 
         // Interface
         this.load.image("background", "assets/Interface/background.png");
-        this.load.image("exitGame", "assets/Interface/ExitGame.svg");
-        this.load.image("exitGame", "assets/Interface/Themes.svg");
-        this.load.image("exitGame", "assets/Interface/Profile.svg");
-        this.load.image("exitGame", "assets/Interface/Bag.svg");
-        this.load.image("exitGame", "assets/Interface/List.svg");
+        this.load.image("exitBtn", "assets/Interface/ExitGame.svg");
+        this.load.image("themeBtn", "assets/Interface/Themes.svg");
+        this.load.image("profileBtn", "assets/Interface/Profile.svg");
+        this.load.image("bagBtn", "assets/Interface/Bag.svg");
+        this.load.image("listBtn", "assets/Interface/List.svg");
         this.load.image("exitGame", "assets/Interface/Profile.svg");
 
         this.load.image("char1", "assets/Spaceroom/Characters/character_1.svg");
@@ -30,11 +30,18 @@ export class PartyInterface extends Phaser.Scene {
 
     create() {
         //this.add.image(400, 300, CST.IMAGES.TEST_CHARACTER);
+
+        // background
         this.background = this.add.image(0,76,"background");
         this.background.setOrigin(0,0);
         this.background.displayWidth=this.game.config.width;
         this.background.scaleY=this.background.scaleX;
 
+        console.log("e")
+        //this.exitBtn = this.add.image()
+
+
+        // 
         this.image1 = this.add.image(400,300,"char1");
         this.image2 = this.add.image(400,400,"char2");
     }
