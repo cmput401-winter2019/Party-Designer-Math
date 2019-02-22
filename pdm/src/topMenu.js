6941fac5dc776e7662d174a03b8fdd1902fe1a9a
“LeetDit” <heesoo@ualberta.ca>
2019-02-22 16:53:37 -0700

    topMenu.js doesn't work

---
+++ pdm/src/topMenu.js
@@ -0,0 +1,7 @@
+export default class TopMenu extends Phaser.GameObjects.Container{ 
+    constructor(config){
+		super(config.scene);
+        this.scene = config.scene;
+        //this.exitBtn = this.scene.add.image(30,30,"exitBtn");
+    }
+}
