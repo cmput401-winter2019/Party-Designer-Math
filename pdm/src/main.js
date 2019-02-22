f515ac083b5d82b572c941d5c2f1178d19c96fb3
HailanXyouknow <hailan@ualberta.ca>
2019-02-22 16:05:11 -0700

    background set

--- pdm/src/main.js
+++ pdm/src/main.js
@@ -1,27 +1,27 @@
+var game;
 import {PartyInterface} from "./scenes/PartyInterface"
 
-var game;
-
-var isMobile = navigator.userAgent.indexOf("Mobile");
-if (isMobile == -1) {
-    isMobile = navigator.userAgent.indexOf("Tablet");
+window.onload=function(){
+    var isMobile = navigator.userAgent.indexOf("Mobile");
+    if (isMobile == -1) {
+        isMobile = navigator.userAgent.indexOf("Tablet");
+    }
+    if (isMobile ==-1){
+        var config = {
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            parent: 'phaser-game',
+            scene: [PartyInterface]
+        };
+    } else {
+        var config = {
+            type: Phaser.AUTO,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            parent: 'phaser-game',
+            scene: [PartyInterface]
+        };
+    }
+    game = new Phaser.Game(config);
 }
-if (isMobile ==-1){
-    var config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        parent: 'phaser-game',
-        scene: [PartyInterface]
-    };
-} else {
-    var config = {
-        type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        parent: 'phaser-game',
-        scene: [PartyInterface]
-    };
-}
-
-game = new Phaser.Game(config);
