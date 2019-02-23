f215cec4c09f85e2854835550d7cb08779ae08b1
“LeetDit” <heesoo@ualberta.ca>
2019-02-22 20:43:09 -0700

    top icons

--- pdm/src/scenes/PartyInterface.js
+++ pdm/src/scenes/PartyInterface.js
@@ -1,6 +1,6 @@
 import { CST } from "../CST";
-import { TopMenu } from "../components/topMenu";
-//import { Bar } from "bar"
+import { ButtonAtMenu } from "../components/buttonAtMenu";
+
 export class PartyInterface extends Phaser.Scene {
 
     constructor() {
@@ -18,6 +18,7 @@ export class PartyInterface extends Phaser.Scene {
         // Interface
         this.load.image("background", "assets/Interface/background.png");
         this.load.image("exitBtn", "assets/Interface/ExitGame.svg");
+        this.load.image("saveBtn", "assets/Interface/Save.svg");
         this.load.image("themeBtn", "assets/Interface/Themes.svg");
         this.load.image("profileBtn", "assets/Interface/Profile.svg");
         this.load.image("bagBtn", "assets/Interface/Bag.svg");
@@ -38,13 +39,18 @@ export class PartyInterface extends Phaser.Scene {
         this.background.displayWidth=this.game.config.width;
         this.background.scaleY=this.background.scaleX;
 
-        this.topMenu = new TopMenu({scene:this});
-        
-        // this.themeBtn = this.add.image(0,76,"themeBtn");
-        // this.rofileBtn = this.add.image(0,76,"profileBtn");
-        // this.bagBtn = this.add.image(0,76,"bagBtn");
-        // this.listBtn = this.add.image(0,76,"listBtn");
-        // this.exitGame = this.add.image(0,76,"exitGame");
+
+        // Top menu buttons
+        var fireText = 
+        this.exitBtn = new ButtonAtMenu({scene:this, key:"exitBtn", text:"Exit Game", x: (this.game.config.width*0.05), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.themeBtn = new ButtonAtMenu({scene:this, key:"themeBtn", text:"Themes", x: (this.game.config.width*0.12), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.saveBtn = new ButtonAtMenu({scene:this, key:"saveBtn", text:"Save", x: (this.game.config.width*0.19), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.profileBtn = new ButtonAtMenu({scene:this, key:"profileBtn", text:"Profile", x: (this.game.config.width*0.26), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.bagBtn = new ButtonAtMenu({scene:this, key:"bagBtn", text:"Bag", x: (this.game.config.width*(1-0.26)), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.listBtn = new ButtonAtMenu({scene:this, key:"listBtn", text:"List", x: (this.game.config.width*(1-0.19)), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.creditBtn = new ButtonAtMenu({scene:this, key:"creditBtn", text:"Credits", x: (this.game.config.width*(1-0.12)), y:30, event: 'button_pressed', params:'self_destruct'});
+        this.textConfig = {fontFamily:'Muli', color:'#ffffff', fontSize:'30px'};
+		this.text1=this.add.text((this.game.config.width*(1-0.10)),14,"200.00", this.textConfig);
 
         this.image1 = this.add.image(400,300,"char1");
         this.image2 = this.add.image(400,400,"char2");
