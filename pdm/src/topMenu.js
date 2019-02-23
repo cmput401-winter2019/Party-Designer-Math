5b03d553717606ac1a147254657487c56cdf9965
“LeetDit” <heesoo@ualberta.ca>
2019-02-22 17:10:16 -0700

    top menu icons are showing

--- pdm/src/topMenu.js
+++ pdm/src/topMenu.js
@@ -3,5 +3,10 @@ export class TopMenu extends Phaser.GameObjects.Container{
 		super(config.scene);
         this.scene = config.scene;
         this.exitBtn = this.scene.add.image(30,30,"exitBtn");
+        this.themeBtn = this.scene.add.image(60,30,"themeBtn");
+        this.profileBtn = this.scene.add.image(90,30,"profileBtn");
+        this.bagBtn = this.scene.add.image(120,30,"bagBtn");
+        this.listBtn = this.scene.add.image(150,30, "listBtn");
+        this.creditBtn = this.scene.add.image(180,30,"creditBtn");
     }
 }
