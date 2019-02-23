efe8d02c069654b93bba7f560909da91516328af
HailanXyouknow <hailan@ualberta.ca>
2019-02-23 00:24:07 -0700

    edit guest button shows after long hold

--- pdm/src/components/guests.js
+++ pdm/src/components/guests.js
@@ -10,7 +10,27 @@ export class Guest extends Phaser.GameObjects.Sprite{
 
 		this.name = name;
         this.type = "guest";
+        this.customize = false;
 
+        // ---- guest buttons -----
+        this.rotateBtn=this.scene.add.image(this.x+(this.displayWidth/2), this.y,'rotateBtn');
+        this.rightBtn=this.scene.add.image(this.x+(this.displayWidth/2), this.y+this.rotateBtn.displayHeight,'rightBtn');
+        this.rotateBtn.visible = false;
+        this.rightBtn.visible = false;
+        this.rightBtn.setInteractive();
+        this.rotateBtn.setInteractive();
+        this.rightBtn.on('pointerdown', this.hideButtons, this);
+
+        this.scene.input.on('pointerup', function(pointer){
+            var duration = pointer.getDuration();
+            if (duration > 800){
+                console.log("show buttons");
+                if(this.customize == false){
+                    this.showButtons();
+                    this.customize = true;
+                }
+            }
+        },this);
 
 
         // ------ drag logic ------
@@ -26,5 +46,18 @@ export class Guest extends Phaser.GameObjects.Sprite{
             } 
         });
         // ------ ------ ------
+    }
+    showButtons(){
+        this.rotateBtn.x = this.x+(this.displayWidth/2);
+        this.rotateBtn.y = this.y;
+        this.rightBtn.x = this.rotateBtn.x;
+        this.rightBtn.y = this.y+this.rotateBtn.displayHeight;
+        this.rotateBtn.visible = true;
+        this.rightBtn.visible = true;    
+    }
+    hideButtons(){
+        this.rotateBtn.visible = false;
+        this.rightBtn.visible = false;   
+        this.customize = false;
     }
 }
