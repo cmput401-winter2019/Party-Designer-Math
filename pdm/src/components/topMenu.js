808a773e90ea3997a8beddc4b664bae934d6d2b5
HailanXyouknow <hailan@ualberta.ca>
2019-02-22 18:05:45 -0700

    organized topMenu (components directory)

--- pdm/src/components/topMenu.js
+++ pdm/src/components/topMenu.js
@@ -1,7 +1,10 @@
-export class TopMenu extends Phaser.GameObjects.Container{ 
+import { ButtonAtMenu } from "./buttonAtMenu";
+export class TopMenu{ 
     constructor(config){
-		super(config.scene);
+		//super(config.scene);
         this.scene = config.scene;
+
+        //this.exitBtn = new ButtonAtMenu({scene:this, key:'exitBtn', text:"Exit Button!", x: 300, y:30, event: 'button_pressed', params:'self_destruct'});
         this.exitBtn = this.scene.add.image(30,30,"exitBtn");
         this.themeBtn = this.scene.add.image(60,30,"themeBtn");
         this.profileBtn = this.scene.add.image(90,30,"profileBtn");
