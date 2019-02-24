bcc5e2d4eec29cefc0c8577398c6e4257709baf5
“LeetDit” <heesoo@ualberta.ca>
2019-02-23 19:13:56 -0700

    structure for bottom button press

--- pdm/src/components/btnAtBottom.js
+++ pdm/src/components/btnAtBottom.js
@@ -12,18 +12,19 @@ export class BtnAtBottom extends Phaser.GameObjects.Container{
         this.startHeight = config.startHeight;
         this.btnHeight = config.btnHeight;
         this.btnWidth = config.btnWidth;
+        this.btnColor = config.btnColor;
 
         // Draw Rectangle
-        this.rect = this.scene.add.rectangle(0, this.startHeight, this.btnWidth, this.btnHeight, 0x0e4361); // x, y, width, height
+        this.rect = this.scene.add.rectangle(0, this.startHeight, this.btnWidth, this.btnHeight, this.btnColor); // x, y, width, height
         this.rect.setOrigin(0,0);
         this.rect.setStrokeStyle(0.75, 0xffffff);
         this.add(this.rect);
         
         // Add text
 		this.textConfig = {fontFamily:'Muli', color:'#ffffff', fontSize:'20px'};
-		this.text1=this.scene.add.text(this.btnWidth*0.25,this.startHeight+(this.btnHeight/2), config.text, this.textConfig);
-		this.text1.setOrigin(0,0.5);
-		this.add(this.text1);
+		this.text=this.scene.add.text(this.btnWidth*0.25,this.startHeight+(this.btnHeight/2), config.text, this.textConfig);
+		this.text.setOrigin(0,0.5);
+		this.add(this.text);
 
 		// Button has mouse over effect on desktop computers
         this.setSize(this.btnWidth, this.btnHeight);
@@ -34,9 +35,25 @@ export class BtnAtBottom extends Phaser.GameObjects.Container{
         this.rect.on('pointerout', function(pointer){
             this.alpha = 1;
         }, this);
+        this.rect.on('pointerdown', this.activateBtn, this);
 
         this.scene.add.existing(this);
     
+    }
+    activateBtn(){
+    
+        if(this.text._text == 'Furniture'){
+            console.log(this.text._text);
+        }
+        else if (this.text._text ==  "Decoration"){
+            console.log(this.text._text);
+        }
+        else if (this.text._text ==  "Snacks"){
+            console.log(this.text._text);
+        }
+        else if (this.text._text == "Kiddie Bag"){
+            console.log(this.text._text);
+        }
     }
 
 
