3b2a72b30b07df4ccd1d3e6a0b1b374d4bf31b48
Hussain <hnkhan@ualberta.ca>
2019-02-25 19:49:54 -0700

    Loading snacks

--- pdm/src/CST.js
+++ pdm/src/CST.js
@@ -5,5 +5,18 @@ export const CST = {
 
     IMAGES: {
         TEST_CHARACTER: "assets/CHARACTER_5.svg"
+    },
+
+    SNACKS: {
+        CHIPS: "chips",
+        BURGER: "burger",
+        BURGERS: "burgers",
+        CHERRIES: "cherries",
+        WATER: "water",
+        SALAD: "salad",
+        PIZZA: "pizza",
+        JUICE: "juice",
+        KETCHUP: "ketchup",
+        MILKSHAKE: "milkshake"
     }
 }
