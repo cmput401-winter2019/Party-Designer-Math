8a3faa733e7ca35c73be0ea5911ddd1d67522d64
“LeetDit” <heesoo@ualberta.ca>
2019-02-28 02:03:04 -0700

    added checkboxes

--- pdm/src/scenes/ShoppingInterface.js
+++ pdm/src/scenes/ShoppingInterface.js
@@ -22,12 +22,38 @@ export class ShoppingInterface extends Phaser.Scene{
             
 
             var graphics = this.add.graphics();
+            var graphicsCheck = this.add.graphics();
+            var graphicsborder= this.add.graphics();
+            
+            
             graphics.fillStyle(0xFFFFFF, 1.0);
             graphics.fillRect(200, 100, 400, 200);
-            this.add.text(320, 270, 'Click to Return', { font: '16px Courier', fill: '0xFFFFF' });
-            this.add.text(220, 120, 'Buy '+ Phaser.Math.Between(1,9) + ' Burgers' ,{ font: '16px Courier', fill: '0xFFFFF' });
-            this.add.text(220, 150, 'Buy '+ Phaser.Math.Between(1,9) + ' Condoms', { font: '16px Courier', fill: '0xFFFFF' });
-            this.add.text(220, 180, 'Buy '+ Phaser.Math.Between(1,9) + ' Chips', { font: '16px Courier', fill: '0xFFFFF' });
+
+            
+            graphicsCheck.fillStyle(0x00000, 1.0);
+            graphicsCheck.fillRect(220,118,20,20);
+            graphicsborder.fillStyle(0xFFFFFF, 1.0);
+            graphicsborder.fillRect(223,121,13,13);
+
+
+            graphicsCheck.fillStyle(0x00000, 1.0);
+            graphicsCheck.fillRect(220,148,20,20);
+            graphicsborder.fillStyle(0xFFFFFF, 1.0);
+            graphicsborder.fillRect(223,151,13,13);
+
+
+
+            graphicsCheck.fillStyle(0x00000, 1.0);
+            graphicsCheck.fillRect(220,178,20,20);
+            graphicsborder.fillStyle(0xFFFFFF, 1.0);
+            graphicsborder.fillRect(223,181,13,13);
+
+
+
+            this.add.text(370, 270, 'Click to Return', { font: '16px Courier', fill: '0xFFFFF' });
+            this.add.text(270, 120, 'Buy '+ Phaser.Math.Between(1,9) + ' Burgers' ,{ font: '16px Courier', fill: '0xFFFFF' });
+            this.add.text(270, 150, 'Buy '+ Phaser.Math.Between(1,9) + ' Condoms', { font: '16px Courier', fill: '0xFFFFF' });
+            this.add.text(270, 180, 'Buy '+ Phaser.Math.Between(1,9) + ' Chips', { font: '16px Courier', fill: '0xFFFFF' });
     
             this.input.once('pointerdown', function () {
     
