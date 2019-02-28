b96c456d020e2f9e5208df2a38d5a3b91753a5a9
HailanXyouknow <hailan@ualberta.ca>
2019-02-28 11:08:28 -0700

    fontConfig in ShoppingInterface

--- pdm/src/scenes/ShoppingInterface.js
+++ pdm/src/scenes/ShoppingInterface.js
@@ -21,42 +21,33 @@ export class ShoppingInterface extends Phaser.Scene{
         }
 
         create(){
-            
-            
-
             var graphics = this.add.graphics();
             var graphicsCheck = this.add.graphics();
             var graphicsborder= this.add.graphics();
             
-            
             graphics.fillStyle(0xFFFFFF, 1.0);
             graphics.fillRect(200, 100, 400, 200);
-
             
             graphicsCheck.fillStyle(0x00000, 1.0);
             graphicsCheck.fillRect(220,118,20,20);
             graphicsborder.fillStyle(0xFFFFFF, 1.0);
             graphicsborder.fillRect(223,121,13,13);
 
-
             graphicsCheck.fillStyle(0x00000, 1.0);
             graphicsCheck.fillRect(220,148,20,20);
             graphicsborder.fillStyle(0xFFFFFF, 1.0);
             graphicsborder.fillRect(223,151,13,13);
-
-
 
             graphicsCheck.fillStyle(0x00000, 1.0);
             graphicsCheck.fillRect(220,178,20,20);
             graphicsborder.fillStyle(0xFFFFFF, 1.0);
             graphicsborder.fillRect(223,181,13,13);
 
-
-
-            this.add.text(370, 270, 'Click to Return', { font: '16px Courier', fill: '0xFFFFF' });
-            this.add.text(270, 120, 'Buy '+ this.number1 + ' Burgers' ,{ font: '16px Courier', fill: '0xFFFFF' });
-            this.add.text(270, 150, 'Buy '+ this.number2 + ' Condoms', { font: '16px Courier', fill: '0xFFFFF' });
-            this.add.text(270, 180, 'Buy '+ this.number3 + ' Chips', { font: '16px Courier', fill: '0xFFFFF' });
+            var fontConfig = { font: '16px Muli', fill: '0xFFFFF' };
+            this.add.text(370, 270, 'Click to Return', fontConfig);
+            this.add.text(270, 120, 'Buy '+ this.number1 + ' Burgers', fontConfig);
+            this.add.text(270, 150, 'Buy '+ this.number2 + ' Condoms', fontConfig);
+            this.add.text(270, 180, 'Buy '+ this.number3 + ' Chips', fontConfig);
     
             this.input.on('pointerdown', function () {
                 console.log("ok");
