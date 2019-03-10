e9a644e920f70a83f2e778896b350e33a0e1bac0
“LeetDit” <heesoo@ualberta.ca>
2019-03-10 12:23:16 -0600

    question now looks the same as buy window

--- pdm/src/components/Question.js
+++ pdm/src/components/Question.js
@@ -1,24 +1,28 @@
+import { ImageToProperties } from "../classes/imageToProperties";
+
 export class Question extends Phaser.GameObjects.Container{ 
     constructor(scene, name, amount){
         super(scene);
         //Initialize members
         this.scene = scene;
         this.name = name;
+        this.imageToProp = new ImageToProperties();
+        this.pluralName = this.imageToProp.getProp(this.name).pluralName;
         this.amount = amount;
 
         //Configurations for text
-        this.textConfig = {fontFamily:'Muli', color:'#ffffff', fontSize:'12px'};
+        this.textConfig = {fontFamily:'Muli', color:'#000000', fontSize:'12px'};
 
-        this.questionBackground = this.scene.add.rectangle(1, 1, 300, 90, 0x0e4361);
-        //this.questionBackground.setOrigin(0, 0);
-        this.questionBackground.setStrokeStyle(1.5, 0xffffff);
-        this.questionText = this.scene.add.text(65-145, 20-45, "How much would it cost to buy\n" + this.amount + " " + this.name + "?", this.textConfig);
-        //this.questionText.setOrigin(0, 0);
+        this.questionBackground = this.scene.add.rectangle(0, 0, this.scene.game.config.width*0.4, 90, 0xffffff);
+        this.questionBackground.setOrigin(0.5,0.5);
+        this.questionBackground.setStrokeStyle(1.5, 0x000000);
+        this.questionText = this.scene.add.text(0, 0-20, "How much would it cost to buy " + this.amount + " " + this.pluralName + "?", this.textConfig);
+        this.questionText.setOrigin(0.5, 0.5);
 
-        this.questionSubmitBackground = this.scene.add.rectangle(150-145, 65-45, 55, 15, 0x0e4361);
-        this.questionSubmitBackground.setStrokeStyle(1.5, 0xffffff);
-        this.questionSubmitText = this.scene.add.text(127-145, 57-45, "SUBMIT", this.textConfig);
-        //this.questionSubmitText.setOrigin(0,0);
+        this.questionSubmitBackground = this.scene.add.rectangle(0, 0+25, 55, 15, 0x02C2FF);
+        this.questionSubmitBackground.setStrokeStyle(1.5, 0xB2B3B4).setOrigin(0.5,0.5);
+        this.questionSubmitText = this.scene.add.text(0, 0+25, "SUBMIT", {fontFamily:'Muli', color:'#ffffff', fontSize:'12px'});
+        this.questionSubmitText.setOrigin(0.5,0.5);
 
         this.add(this.questionBackground);
         this.add(this.questionText);
