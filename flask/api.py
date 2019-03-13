8c85220888e358515e090be28f79c723a34d1d66
“LeetDit” <heesoo@ualberta.ca>
2019-03-13 14:36:33 -0400

    -Gamestate API and model -Added migrations -SQLite? 1to1 not working

--- flask/api.py
+++ flask/api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
-from serializers import StudentSerializer, ma
-from models import Student, db
-import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from serializers import StudentSerializer, GameStateSerializer, ma
+from models import Student, GameState, Bag, db
+import os, sys
 
 # create app
 app = Flask(__name__)
@@ -11,34 +13,72 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 ma.init_app(app)
 
 # create serializers for student(s)
 student_serializer = StudentSerializer()
 students_serializer = StudentSerializer(many=True)
 
+#create serializer for gamestate
+gameStateSerializer = GameStateSerializer()
+gameStatesSerializer = GameStateSerializer(many=True)
+
 # endpoint to create new student
 @app.route("/student", methods=["POST"])
 def add_student():
     try:
         name = request.json['name']
-        classcode = request.json['classcode']
+        classCode = request.json['classCode']
         
-        new_student = Student(name, classcode)
+        newStudent = Student(name, classCode)
 
-        db.session.add(new_student)
+        db.session.add(newStudent)
         db.session.commit()
 
         return jsonify(success=True), 201
-    except:
+    except Exception as e:
+        print(e)
         return jsonify(success=False), 403
 
 # endpoint to show all students
 @app.route("/student", methods=["GET"])
 def get_student():
-    all_students = Student.query.all()
-    result = students_serializer.dump(all_students)
+    allStudents = Student.query.all()
+    result = students_serializer.dump(allStudents)
+    return jsonify(result.data)
+
+
+# endpoint to create game state for student
+@app.route("/<id>/gamestate", methods=["POST"])
+def add_gamestate(id):
+    try:
+        money = request.json['money']
+        numOfGuests = request.json['numOfGuests']
+        studentId = id
+        
+        newGameState = GameState(money, numOfGuests, studentId)
+
+        db.session.add(newGameState)
+        db.session.commit()
+
+        return jsonify(success=True), 201
+    except Exception as e:
+        print(e)
+        return jsonify(success=False), 403
+
+# endpoint to show game state of student
+@app.route("/<id>/gamestate", methods=["GET"])
+def get_gamestate(id):
+    gameState = GameState.query.all()
+    #gameState = GameState.query.filter(GameState.studentId == id).all()
+    result = gameStatesSerializer.dump(gameState)
+    print(result.data)
     return jsonify(result.data)
 
 if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        manager.run()
     app.run(debug=True, port=5001)
