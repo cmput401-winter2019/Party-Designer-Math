0b1a8cafdec19daa95ccefdae9450ed66b9bde19
“LeetDit” <heesoo@ualberta.ca>
2019-03-13 22:07:38 -0400

    -Added models & serializers -Added place holder for question generation

--- flask/api.py
+++ flask/api.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from serializers import StudentSerializer, GameStateSerializer, BagItemSerializer, CanvasItemSerializer, ma
-from models import Student, GameState, BagItem, CanvasItem, db
+from serializers import StudentSerializer, GameStateSerializer, BagItemSerializer, CanvasItemSerializer, QuestionSerializer, ma
+from models import Student, GameState, BagItem, CanvasItem, Question, db
 import os, sys
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from sqlite3 import Connection as SQLite3Connection
+from questiongen import QuestionGenerator
 
 # create app
 app = Flask(__name__)
@@ -34,17 +35,21 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
 studentSerializer = StudentSerializer()
 studentsSerializer = StudentSerializer(many=True)
 
-# create serializer for game state
+# create serializer for game state(s)
 gameStateSerializer = GameStateSerializer()
 gameStatesSerializer = GameStateSerializer(many=True)
 
-# create serializer for bag item
+# create serializer for bag item(s)
 bagItemSerializer = BagItemSerializer()
 bagItemsSerializer = BagItemSerializer(many=True)
 
-# create serializer for canvas item
+# create serializer for canvas item(s)
 canvasItemSerializer = BagItemSerializer()
 canvasItemsSerializer = BagItemSerializer(many=True)
+
+# create serializer for question(s)
+questionSerializer = QuestionSerializer()
+questionsSerializer = QuestionSerializer(many=True)
 
 # endpoint to create new student
 @app.route("/student", methods=["POST"])
@@ -69,7 +74,6 @@ def get_student():
     allStudents = Student.query.all()
     result = studentsSerializer.dump(allStudents)
     return jsonify(result.data)
-
 
 # endpoint to create game state for student
 @app.route("/<id>/gamestate", methods=["POST"])
@@ -114,13 +118,12 @@ def add_bagitem(id):
         print(e)
         return jsonify(success=False), 403
 
-# endpoint to show bag items of gamestate
+# endpoint to show bag items of game state
 @app.route("/<id>/bagitem", methods=["GET"])
 def get_bagitems(id):
     bagItems = BagItem.query.filter(BagItem.gameStateId == id).all()
     result = bagItemsSerializer.dump(bagItems)
     return jsonify(result.data)
-
 
 # endpoint to create canvas item for game state
 @app.route("/<id>/bagitem", methods=["POST"])
@@ -140,12 +143,41 @@ def add_canvasitem(id):
         print(e)
         return jsonify(success=False), 403
 
-# endpoint to show canvas items of gamestate
+# endpoint to show canvas items of game state
 @app.route("/<id>/bag", methods=["GET"])
 def get_canvasitems(id):
     canvasItems = CanvasItem.query.filter(CanvasItem.gameStateId == id).all()
     result = canvasItemsSerializer.dump(canvasItems)
     return jsonify(result.data)
+
+# endpoint to create question for game state
+@app.route("/<id>/question", methods=["POST"])
+def add_question(id):
+    try:
+        gameStateId = id
+        
+        newQuestion = Question(gameStateId)
+
+        db.session.add(newQuestion)
+        db.session.commit()
+
+        # TODO: generate the question here using 
+        itemName = None
+        itemAmount = None
+        questionGenerator = QuestionGenerator(itemName, itemAmount)
+
+        return jsonify(success=True), 201
+    except Exception as e:
+        print(e)
+        return jsonify(success=False), 403
+
+# endpoint to show question of game state
+@app.route("/<id>/question", methods=["GET"])
+def get_question(id):
+    questions = Question.query.filter(Question.gameStateId == id).all()
+    result = questionsSerializer.dump(questions)
+    return jsonify(result.data)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
