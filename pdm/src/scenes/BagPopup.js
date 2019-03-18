952d2fa31d954cfb340c01b59e9a5fd0c15b8e5d
“LeetDit” <heesoo@ualberta.ca>
2019-03-17 18:08:09 -0600

    item put into backpack now shows in backpack

--- pdm/src/scenes/BagPopup.js
+++ pdm/src/scenes/BagPopup.js
@@ -1,7 +1,8 @@
 import {CST} from "../CST";
 import { AlignGrid } from "../util/alignGrid";
+import { ImageToProperties } from "../classes/imageToProperties";
 
-export class BuyPopup extends Phaser.Scene{
+export class BagPopup extends Phaser.Scene{
 
     constructor(){
         super({
@@ -11,7 +12,7 @@ export class BuyPopup extends Phaser.Scene{
     init(data)
     {
         // this.objName = data.objName;
-        // this.originalS = data.originalS;
+        this.player = data.player;
     }
     preload()
     {
@@ -31,12 +32,47 @@ export class BuyPopup extends Phaser.Scene{
         //this.alignGrid.showNumbers();
         var fontConfig = { font: '16px Muli', fill: '0x000000' };
          
-        //this.buyItemPopup = new BuyItem(this, this.objName, this.originalS);
+        // Main rectangle of the scene
+        this.popupHeight = 540;
+        this.rect = this.add.rectangle(0, 0, this.game.config.width*0.9, this.popupHeight, 0xffffff);
+        this.rect.setOrigin(0.5,0.5);
+        this.rect.setStrokeStyle(2, 0x000000);
+        this.alignGrid.placeAtIndex(220, this.rect);
 
-        // this.question = new Question(this, "chair", 2);
+        for (var key in this.player.backpack){
+            console.log(key, this.player.backpack[key]);
+        }
 
-        // this.buyItemPopup.setInteractive();
-        // this.input.setDraggable(this.buyItemPopup);
+        // Configs
+        var centerXY = this.alignGrid.getPosByIndex(220);
+        var fontConfig = { font: '16px Muli', fill: '0xFFFFF' };
 
+        // Initiate ImageToProperties class
+        this.imageToProp = new ImageToProperties();
+        // List item (key-value pairs) in player's backpack
+        var i = 0;
+        for(var key in this.player.backpack){
+            var text = this.add.text(this.game.config.width*0.1, (centerXY.y-150)+(i*30), this.imageToProp.getProp(key).name, fontConfig);
+            var text2 = this.add.text(this.game.config.width*0.1+150, (centerXY.y-150)+(i*30), this.player.backpack[key], fontConfig);
+            var asset = this.add.image(this.game.config.width*0.35, (centerXY.y-150)+(i*30), key);
+            text.setOrigin(0,0.5);
+            text2.setOrigin(0,0.5);
+            asset.displayWidth = 30;
+            asset.scaleY = asset.scaleX;
+            if (asset.displayHeight > 30){
+                asset.displayHeight = 30;
+                asset.scaleX = asset.scaleY;
+            }
+            i++;
+        }
+
+        this.add.text(this.game.config.width*0.1, centerXY.y-250, 'Backpack', { font: '20px Muli', fill: '0xFFFFF' });
+        this.add.text(this.game.config.width*0.1, centerXY.y-200, 'ITEM NAME' + '           '+ 'OWNED', { font: '17px Muli', fill: '0xFFFFF' });
+        this.returnBtn = this.add.text(this.game.config.width*0.75, centerXY.y+220, 'Click Here to Return', fontConfig);
+        this.returnBtn.setInteractive();
+        this.returnBtn.on('pointerdown', function () {
+            this.scene.sleep(CST.SCENES.BAG_POPUP);
+        }, this);
     }
+
 }
