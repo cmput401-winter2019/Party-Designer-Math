80e5c78c2eaedb9ca46e6cb788b78f88df3ea83a
Hussain <hnkhan@ualberta.ca>
2019-03-25 20:09:55 -0600

    Added signup page

--- frontend/build.ps1
+++ frontend/build.ps1
@@ -4,4 +4,4 @@ Start-Sleep -s 1
 New-Item ./dist -ItemType directory -Force
 Start-Sleep -s 1
 Copy-Item ./assets/ ./dist/ -Recurse -Force
-parcel ./templates/login.html ./templates/play.html
+parcel ./templates/login.html ./templates/play.html ./templates/signup.html
