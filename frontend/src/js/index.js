ef2d32d41bbc3a49febe053cb1b2fc3d32180816
HailanXyouknow <hailan@ualberta.ca>
2019-03-30 12:51:41 -0600

    level up scene

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,13 +1,14 @@
-import config             from './Config/config'
-import { BootScene }      from './Scenes/BootScene';
-import { ChooseTheme }     from './Scenes/ChooseTheme';
-import { PreloaderScene } from './Scenes/PreloaderScene';
-import { LoginScene }     from './Scenes/LoginScene';
-import { GameScene }      from './Scenes/GameScene';
-import { PartyInvitation } from "./Scenes/PartyInvitation";
-import { ShoppingInterface } from "./Scenes/ShoppingInterface";
-import { BuyPopup } from "./Scenes/BuyPopup";
-import { BagPopup } from "./Scenes/BagPopup";
+import config                 from './Config/config'
+import { BootScene }          from './Scenes/BootScene';
+import { ChooseTheme }        from './Scenes/ChooseTheme';
+import { PreloaderScene }     from './Scenes/PreloaderScene';
+import { LoginScene }         from './Scenes/LoginScene';
+import { GameScene }          from './Scenes/GameScene';
+import { PartyInvitation }    from "./Scenes/PartyInvitation";
+import { ShoppingInterface }  from "./Scenes/ShoppingInterface";
+import { BuyPopup }           from "./Scenes/BuyPopup";
+import { BagPopup }           from "./Scenes/BagPopup";
+import { LevelUpScene }       from "./Scenes/LevelUpScene";
 
 
 class Game extends Phaser.Game{
@@ -22,6 +23,7 @@ class Game extends Phaser.Game{
     this.scene.add("SHOPPINT_INTERFACE", ShoppingInterface);
     this.scene.add("BUY_POPUP", BuyPopup);
     this.scene.add("BAG_POPUP", BagPopup);
+    this.scene.add("LEVEL_UP", LevelUpScene);
     this.scene.start("BOOT");
   }
 }
