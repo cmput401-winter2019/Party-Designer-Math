302d8b31273e4b99dbc55a9c30805d9312935091
Hee Soo Park <heesoopark@Hees-MacBook-Pro.local>
2019-03-31 19:27:06 -0600

    heesoo

--- frontend/src/js/classes/item.js
+++ frontend/src/js/classes/item.js
@@ -3,66 +3,64 @@ import { CheckToBackpack } from "../Components/checkToBackpack";
 export class Item extends Phaser.GameObjects.Sprite{
     constructor(scene, image, x, y, name, pluralName, category, cost, unit, purpose){
         super(scene);
-		this.scene = scene;
-		this.scene.add.existing(this);
-		this.setTexture(image);
-		this.setPosition(x,y);
-		this.setInteractive();
-		this.scene.input.setDraggable(this);
+
+      	this.scene = scene;
+      	this.scene.add.existing(this);
+
+      	this.setTexture(image);
+      	this.setPosition(x,y);
+      	this.setInteractive();
+
+      	this.scene.input.setDraggable(this);
 
         // Item properties
-		this.name = name;
-        this.imageName = image;
-        this.type = "item";
-        this.angle = 0;
-        this.pluralName = pluralName;
-        this.category = category;
-        this.cost = cost;
-        this.unit = unit;
-        this.purpose = purpose;
+      	this.name         = name;
+        this.imageName    = image;
+        this.type         = "item";
+        this.angle        = 0;
+        this.pluralName   = pluralName;
+        this.category     = category;
+        this.cost         = cost;
+        this.unit         = unit;
+        this.purpose      = purpose;
 
         this.displayWidth = this.scene.game.config.width*0.07;
-        this.scaleY = this.scaleX;
+        this.scaleY       = this.scaleX;
 
         // ---- Item buttons -----
-        this.rotateBtn = this.scene.add.image(0, 0,'rotateBtn');
-        this.rotateBtn2 = this.scene.add.image(0, 0,'rotateBtn');
-        this.scaleBtn = this.scene.add.image(0,0,'scaleBtn');
-        this.smallerBtn = this.scene.add.image(0,0,'smallerBtn');
-        this.forwardBtn = this.scene.add.image(0,0, 'forwardBtn').setOrigin(0.5,0);
-        this.backwardBtn = this.scene.add.image(0,0, 'backwardBtn').setOrigin(0.5,0);
-        this.rightBtn = this.scene.add.image(0,0,'rightBtn').setOrigin(0.5,-0.5);
+        this.rotateBtn    = this.scene.add.image(0, 0, 'rotateBtn'  );
+        this.rotateBtn2   = this.scene.add.image(0, 0, 'rotateBtn'  );
+        this.scaleBtn     = this.scene.add.image(0, 0, 'scaleBtn'   );
+        this.smallerBtn   = this.scene.add.image(0, 0, 'smallerBtn' );
+        this.forwardBtn   = this.scene.add.image(0, 0, 'forwardBtn' ).setOrigin(0.5, 0);
+        this.backwardBtn  = this.scene.add.image(0, 0, 'backwardBtn').setOrigin(0.5, 0);
+        this.rightBtn     = this.scene.add.image(0, 0, 'rightBtn'   ).setOrigin(0.5, -0.5);
 
 
-        this.btnList = [this.rotateBtn, this.rotateBtn2, this.smallerBtn, this.scaleBtn, this.forwardBtn, this.backwardBtn, this.rightBtn];
+        this.btnList      = [this.rotateBtn, this.rotateBtn2, this.smallerBtn, this.scaleBtn, this.forwardBtn, this.backwardBtn, this.rightBtn];
 
         // Tranparent background
-        this.rect = this.scene.add.rectangle(0,
-                                            0,
-                                            this.rotateBtn.displayWidth+10,
-                                            this.rotateBtn.displayHeight*7,
-                                            0x3498DB);
-        this.rect.alpha=0.3;
-        this.rect.setOrigin(0.5,0); 
+        this.rect         = this.scene.add.rectangle(0, 0, this.rotateBtn.displayWidth+10, this.rotateBtn.displayHeight*7, 0x3498DB);
+        this.rect.alpha   = 0.3;
+        this.rect.setOrigin(0.5, 0);
         this.hideButtons();
 
         // ---- Set item buttons functions -----
-        for (var i=0; i<this.btnList.length; i++){
-            this.btnList[i].setInteractive();
-        }
+        for (var i=0; i<this.btnList.length; i++){ this.btnList[i].setInteractive(); }
 
-        this.rightBtn.on('pointerdown', this.hideButtons, this);
-        this.rotateBtn.on('pointerdown', this.rotateGuest, this);
-        this.rotateBtn2.on('pointerdown', this.rotateGuest2, this);
-        this.scaleBtn.on('pointerdown', this.biggerGuest, this);
-        this.smallerBtn.on('pointerdown', this.smallerGuest, this);
-        this.forwardBtn.on('pointerdown', this.bringForward, this);
-        this.backwardBtn.on('pointerdown', this.toBackpack, this);
+        this.rightBtn   .on('pointerdown', this.hideButtons,  this);
+        this.rotateBtn  .on('pointerdown', this.rotateGuest,  this);
+        this.rotateBtn2 .on('pointerdown', this.rotateGuest2, this);
+        this.scaleBtn   .on('pointerdown', this.biggerGuest,  this);
+        this.smallerBtn .on('pointerdown', this.smallerGuest, this);
+        this.forwardBtn .on('pointerdown', this.bringForward, this);
+        this.backwardBtn.on('pointerdown', this.toBackpack,   this);
 
         // ---- Item button only shows if hold was not caused by dragging ------
         this.on('pointerdown', function(pointer){
             this.wasDragging = false;
         }, this);
+
         this.on('pointerup', function(pointer){
             var duration = pointer.getDuration();
             if (duration > 300){
@@ -74,9 +72,9 @@ export class Item extends Phaser.GameObjects.Sprite{
 
         if(this.purpose == "load"){
 
-        } else if (this.purpose == "show") {
+        }else if(this.purpose == "show") {
             this.toScreen(this.imageName);
-        } else if (this.purpose == "moveFromBackpack") {
+        }else if(this.purpose == "moveFromBackpack") {
             this.moveFromBackpack(this.imageName);
         }
 
@@ -87,47 +85,45 @@ export class Item extends Phaser.GameObjects.Sprite{
             if(index > -1){
                 this.btnList.splice(index, 1);
             }
-        } else {
+        }else{
             // If item is not in the invitation page, then it is an 'inRoom' item;
             this.inRoom = true;
         }
     }
-    showButtons(){
-        this.alpha = 0.6;
-        this.input.draggable = false;
-        this.btnX = this.x+(this.displayWidth/2);
-        this.btnY = this.y-this.displayHeight/2;
 
+    showButtons(){
+        this.alpha            = 0.6;
+        this.input.draggable  = false;
+        this.btnX             = this.x+(this.displayWidth/2);
+        this.btnY             = this.y-this.displayHeight/2;
 
         for (var i=0; i< this.btnList.length; i++){
-            this.btnList[i].x = this.btnX;
-            this.btnList[i].y = this.btnY+i*this.btnList[i].displayHeight;
+            this.btnList[i].x       = this.btnX;
+            this.btnList[i].y       = this.btnY+i*this.btnList[i].displayHeight;
             this.btnList[i].visible = true;
             this.btnList[i].setDepth(3);
         }
-        
-        this.rect.x = this.rotateBtn.x;
-        this.rect.y = this.rotateBtn.y-this.rotateBtn.displayHeight;
-        this.rect.alpha=0.3;
-        
-        
-
+        this.rect.x     = this.rotateBtn.x;
+        this.rect.y     = this.rotateBtn.y-this.rotateBtn.displayHeight;
+        this.rect.alpha = 0.3;
     }
+
     hideButtons(){
-        this.alpha = 1;
-        this.input.draggable = true;
+        this.alpha            = 1;
+        this.input.draggable  = true;
 
         for (var i=0; i< this.btnList.length; i++){
             this.btnList[i].visible = false;
         }
-
-        this.rect.alpha=0;
+        this.rect.alpha = 0;
     }
+
     destroyButtons(){
         for (var i=0; i< this.btnList.length; i++){
             this.btnList[i].destroy();
         }
     }
+
     rotateGuest(){
         this.angle += 5;
         this.scene.tweens.add({targets: this,duration: 100,y:this.y, angle:this.angle});
@@ -137,28 +133,21 @@ export class Item extends Phaser.GameObjects.Sprite{
         this.scene.tweens.add({targets: this,duration: 100,y:this.y, angle:this.angle});
     }
     biggerGuest(){
-        this.displayWidth+=2;
-        this.scaleY=this.scaleX;
+        this.displayWidth += 2;
+        this.scaleY       = this.scaleX;
     }
     smallerGuest(){
-        this.displayWidth-=2;
-        this.scaleY=this.scaleX;
+        this.displayWidth -=2;
+        this.scaleY       = this.scaleX;
     }
-    bringForward(){
+    bringForward(){}
 
-    }
-    bringBackward(){
+    bringBackward(){}
 
-    }
-    toScreen(imageName){
-        this.scene.player.putIntoScreenItems(imageName);
-        //console.log(this.scene.player);
-    }
-    moveFromBackpack(imageName){
-        this.scene.player.putItemFromBackpackToScreen(imageName);
-    }
-    toBackpack(){
-        this.popup = new CheckToBackpack(this.scene, this);
-    }
+    toScreen(imageName)         { this.scene.player.putIntoScreenItems(imageName); }
+
+    moveFromBackpack(imageName) { this.scene.player.putItemFromBackpackToScreen(imageName); }
+
+    toBackpack()                { this.popup = new CheckToBackpack(this.scene, this); }
 
 }
