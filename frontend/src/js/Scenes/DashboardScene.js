e27ad812a2bd7094e629fff2c819277f9b239af1
Hee Soo Park <heesoopark@Hees-MacBook-Pro.local>
2019-04-03 15:33:23 -0600

    latest

---
+++ frontend/src/js/Scenes/DashboardScene.js
@@ -0,0 +1,14 @@
+import {CST} from "../CST";
+// import { GetClassStudents }        from "../Components/scripts";
+// import { GetUserStat }                  from "../Components/getUserStat";
+
+export class DashboardScene extends Phaser.Scene{
+
+    constructor(){ super({ key: CST.SCENES.DASHBOARD }) }
+
+    preload(){}
+
+    create(){
+      //var url = "http://127.0.0.1:5001/"+ this.player.gamestateId + "/question";
+    }
+}
