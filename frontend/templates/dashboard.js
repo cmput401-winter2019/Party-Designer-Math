7d5bf6a792fbfcb314b22d6fbba0d16108a1e416
“LeetDit” <heesoo@ualberta.ca>
2019-04-05 01:32:31 -0600

    shoppinglist_update

--- frontend/templates/dashboard.js
+++ frontend/templates/dashboard.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import config                       from '../src/js/Config/config'
 import { DashboardScene }           from '../src/js/Scenes/DashboardScene';
-=======
 function populateTable() {
     
->>>>>>> b0e7d41010ddbad1b7cda74ae72cb0ac9933432b
-
 
 }
