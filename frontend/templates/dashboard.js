80f6471040e8de20ad687cf619a626485c530f82
“LeetDit” <heesoo@ualberta.ca>
2019-04-05 03:29:49 -0400

    Bug fix

--- frontend/templates/dashboard.js
+++ frontend/templates/dashboard.js
@@ -34,21 +34,39 @@ async function main() {
         for (const student of data) {
             //Student usernames
             const newRow = studentsInnerTable.insertRow();
-            const newCellUsername  = newRow.insertCell(0);
+            const newCellUsername = newRow.insertCell(0);
             const username  = document.createTextNode(student.username);
             newCellUsername.appendChild(username);
 
             //Student fullname
-            const newCellFullname  = newRow.insertCell(1);
+            const newCellFullname = newRow.insertCell(1);
             const fullname  = document.createTextNode(student.fullname);
             newCellFullname.appendChild(fullname);
 
-            //Student usernames
-            const newCellAddition  = newRow.insertCell(2);
+            //Student addition
+            const newCellAddition = newRow.insertCell(2);
             const addition = document.createTextNode(student.stats.addition  + "%");
             newCellAddition.appendChild(addition);
 
-            console.log(student);
+            //Student subtraction
+            const newCellsubtraction = newRow.insertCell(3);
+            const subtraction = document.createTextNode(student.stats.subtraction  + "%");
+            newCellsubtraction.appendChild(subtraction);
+
+            //Student multiplication
+            const newCellMultiplication = newRow.insertCell(4);
+            const multiplication = document.createTextNode(student.stats.multiplication  + "%");
+            newCellMultiplication.appendChild(multiplication);
+
+            //Student division
+            const newCellDivision = newRow.insertCell(5);
+            const division = document.createTextNode(student.stats.division  + "%");
+            newCellDivision.appendChild(division);
+
+            //Student mixed
+            const newCellMixed = newRow.insertCell(6);
+            const mixed = document.createTextNode(student.stats.mixed  + "%");
+            newCellMixed.appendChild(mixed);
         }
     }
 }
