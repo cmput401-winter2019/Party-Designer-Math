1b26c195ec7488b50cd46f6bfdfd1ef73dee5b50
“LeetDit” <heesoo@ualberta.ca>
2019-04-05 08:20:12 -0600

    last

--- frontend/templates/login.js
+++ frontend/templates/login.js
@@ -22,6 +22,7 @@ function post_request(username, password, loginType) {
         credentials: "same-origin",
         body: JSON.stringify(body),
         headers: {
+          'Access-Control-Allow-Origin':'*',
           "Content-Type": "application/json",
         }
       })
