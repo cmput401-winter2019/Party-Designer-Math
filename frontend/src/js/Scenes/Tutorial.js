394f349f346bb80e775e93dd62a7435675b5746c
“LeetDit” <heesoo@ualberta.ca>
2019-04-08 16:47:59 -0600

    edited the wrong thing last time

--- frontend/src/js/Scenes/Tutorial.js
+++ frontend/src/js/Scenes/Tutorial.js
@@ -51,8 +51,8 @@ export class Tutorial extends Phaser.Scene {
         // Continue Button
         this.click = 0;
         this.continueBtn = new RoundBtn(this,
-                                        this.p1.x+this.p1.displayWidth/2-110,
-                                        this.p1.y-this.p1.displayHeight/8,
+                                        this.p1.x+this.p1.displayWidth/8,
+                                        this.p1.y-this.p1.displayHeight/2+60,
                                         "Next Page",
                                         100,
                                         35);
